23689dcf98f79e00bc6efee874e182490be1a1a6
silentfred <cmptrwizard@gmail.com>
2022-10-06 20:23:35 +0200

    initial commit

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -1,24 +1,29 @@
-const HDHWalletProvider = require('@truffle/hdwallet-provider');
-const {interface, bytecode } = require('compile');
+// https://infura.io/dashboard/ethereum/b8636711061a4f439d6d616cbd84a403/settings
+// https://goerli.etherscan.io/address/0xc0ed2724680188d81abef54869ae9c6e1d04753e
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 
-const provider = new HDHWalletProvider(
+const {abi, evm} = require('../compile');
+
+const provider = new HDWalletProvider(
     '',
     ''
 );
 
 const web3 = new Web3(provider);
 
-
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    console.log('Fetched accounts:', accounts);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Heyho!']})
-        .send({from: accounts[0], gas:1000000});
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({data: evm.bytecode.object, arguments: ['Hi there!']})
+        .send({gas: '1000000', from: accounts[0]});
 
     console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
 };
 
 deploy();
